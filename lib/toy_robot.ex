f38fdb41a8119c5ec0f391b3e2cef0764f30082c
Dawid Dęby <dawid.deby@gmail.com>
2021-03-29 17:58:07 +0200

    Add report function to announce the X,Y and facing of the robot

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -10,4 +10,8 @@ defmodule ToyRobot do
   def place(x, y, facing) do
     %ToyRobot.Position{x: x, y: y, facing: facing}
   end
+
+  def report(%ToyRobot.Position{x: x, y: y, facing: facing}) do
+    {x, y, facing}
+  end
 end
